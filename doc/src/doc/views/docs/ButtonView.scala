a774afbb8e734d1a68f8d7bc63c194a3757c5a8f
nguyenyou <tunt081295@gmail.com>
2025-07-19 18:12:36 +0700

    🚢 ship it

--- doc/src/doc/views/docs/ButtonView.scala
+++ doc/src/doc/views/docs/ButtonView.scala
@@ -43,6 +43,52 @@ object ButtonView
         }
       ),
       Demo(
+        title = "Sizes",
+        description = "Use the `size` attribute to change a button's size.",
+        content = Source.annotate {
+          div(
+            tw.flex.flexWrap.gap2,
+            // <show>
+            Button(_.size := "small")("Small"),
+            Button(_.size := "medium")("Medium"),
+            Button(_.size := "large")("Large")
+            // </show>
+          )
+        }
+      ),
+      Demo(
+        title = "Pill Buttons",
+        description = "Use the `pill` attribute to give buttons rounded edges.",
+        content = Source.annotate {
+          div(
+            tw.flex.flexWrap.gap2,
+            // <show>
+            Button(_.size := "small", _.pill := true)("Small"),
+            Button(_.size := "medium", _.pill := true)("Medium"),
+            Button(_.size := "large", _.pill := true)("Large")
+            // </show>
+          )
+        }
+      ),
+      Demo(
+        title = "Link Buttons",
+        description = """
+        | It's often helpful to have a button that works like a link. 
+        | This is possible by setting the `href` attribute, which will make the component render an `<a>` under the hood. 
+        | This gives you all the default link behavior the browser provides (e.g. `CMD/CTRL/SHIFT + CLICK`) and exposes the `rel`, `target`, and `download` attributes.
+        """.stripMargin,
+        content = Source.annotate {
+          div(
+            tw.flex.flexWrap.gap2,
+            // <show>
+            Button(_.href := "https://example.com/")("Link"),
+            Button(_.href := "https://example.com/", _.target := "_blank")("Link with target"),
+            Button(_.href := "/assets/images/logo.svg", _.download := "shoelace.svg")("Download")
+            // </show>
+          )
+        }
+      ),
+      Demo(
         title = "Icon Buttons",
         content = Source.annotate {
           div(
