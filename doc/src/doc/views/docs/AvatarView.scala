8d3324233dad6da96783b264ad1249d14d3ce2c0
nguyenyou <tunt081295@gmail.com>
2025-07-24 22:35:43 +0700

    🚢 ship it

--- doc/src/doc/views/docs/AvatarView.scala
+++ doc/src/doc/views/docs/AvatarView.scala
@@ -64,6 +64,7 @@ case class AvatarView()
           |""".stripMargin,
         content = Source.annotate {
           div(
+            tw.flex.gap2,
             Avatar(
               _.label := "Avatar with an image icon",
               _.slots.icon(
@@ -90,6 +91,56 @@ case class AvatarView()
                   _.variant := "solid"
                 )()
               )
+            )()
+          )
+        }
+      )().withLocator,
+      Demo(
+        title = "Shapes",
+        description = """
+          | Avatars can be shaped using the `shape` attribute.
+          |""".stripMargin,
+        content = Source.annotate {
+          div(
+            tw.flex.gap2,
+            Avatar(
+              _.shape := "square",
+              _.label := "Square avatar"
+            )(),
+            Avatar(
+              _.shape := "rounded",
+              _.label := "Rounded avatar"
+            )(),
+            Avatar(
+              _.shape := "circle",
+              _.label := "Circle avatar"
+            )()
+          )
+        }
+      )().withLocator,
+      Demo(
+        title = "Avatar Groups",
+        description = """
+          | You can group avatars with a few lines of CSS.
+          |""".stripMargin,
+        content = Source.annotate {
+          div(
+            tw.flex.gap2,
+            Avatar(
+              _.image := "https://images.unsplash.com/photo-1490150028299-bf57d78394e0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=256&h=256&q=80&crop=right",
+              _.label := "Avatar 1 of 4"
+            )(),
+            Avatar(
+              _.image := "https://images.unsplash.com/photo-1503454537195-1dcabb73ffb9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=256&h=256&crop=left&q=80",
+              _.label := "Avatar 2 of 4"
+            )(),
+            Avatar(
+              _.image := "https://images.unsplash.com/photo-1456439663599-95b042d50252?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=256&h=256&crop=left&q=80",
+              _.label := "Avatar 3 of 4"
+            )(),
+            Avatar(
+              _.image := "https://images.unsplash.com/flagged/photo-1554078875-e37cb8b0e27d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=256&h=256&crop=top&q=80",
+              _.label := "Avatar 4 of 4"
             )()
           )
         }
