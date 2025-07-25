bbb046a2fddbf74c4c85c46410aac998e76d5e27
nguyenyou <tunt081295@gmail.com>
2025-07-25 12:08:59 +0700

    🚢 ship it

--- doc/src/doc/views/docs/DialogView.scala
+++ doc/src/doc/views/docs/DialogView.scala
@@ -15,32 +15,97 @@ case class DialogView()
       center = true,
       content = Source.annotate {
         import io.github.nguyenyou.webawesome.laminar.{Dialog, Button}
-        val openVar = Var(false)
+        val openEvent = EventBus[Boolean]()
         div(
           Dialog(
-            _.open <-- openVar.signal,
+            _.open <-- openEvent,
             _.label := "Dialog",
             _.slots.footer(
               Button(
-                _.variant := "brand"
-              )(
-                dataAttr("dialog") := "close",
-                "Close"
-              )
+                _.variant := "brand",
+                _.closeDialog(true)
+              )("Close")
             )
           )("Lorem ipsum dolor sit amet, consectetur adipiscing elit."),
           Button(
-            _.onClick.mapTo(true) --> openVar.writer
-          )(
-            "Open Dialog"
-          )
+            _.onClick.mapTo(true) --> openEvent
+          )("Open Dialog")
         )
       }
     )().withLocator
   }
+
   def component: HtmlElement = {
     div(
+      // Dialog without Header
+      Demo(
+        title = "Dialog without Header",
+        description = "Headers are enabled by default. To render a dialog without a header, add the without-header attribute.",
+        content = Source.annotate {
+          import io.github.nguyenyou.webawesome.laminar.{Dialog, Button}
+          val openEvent = EventBus[Boolean]()
+          div(
+            Dialog(
+              _.open <-- openEvent,
+              _.label := "Dialog",
+              _.withoutHeader := true,
+              _.slots.footer(
+                Button(
+                  _.variant := "brand",
+                  _.closeDialog(true)
+                )("Close")
+              )
+            )("Lorem ipsum dolor sit amet, consectetur adipiscing elit."),
+            Button(
+              _.onClick.mapTo(true) --> openEvent
+            )("Open Dialog")
+          )
+        }
+      )(),
+
+      // Dialog with Footer
+      Demo(
+        title = "Dialog with Footer",
+        description = "Footers can be used to display titles and more. Use the footer slot to add a footer to the dialog.",
+        content = Source.annotate {
+          import io.github.nguyenyou.webawesome.laminar.{Dialog, Button}
+          val openEvent = EventBus[Boolean]()
+          div(
+            Dialog(
+              _.open <-- openEvent,
+              _.label := "Dialog",
+              _.slots.footer(
+                Button(
+                  _.variant := "brand",
+                  _.closeDialog(true)
+                )("Close")
+              )
+            )("Lorem ipsum dolor sit amet, consectetur adipiscing elit."),
+            Button(
+              _.onClick.mapTo(true) --> openEvent
+            )("Open Dialog")
+          )
+        }
+      )(),
+
+      Demo(
+        title = "Opening and Closing Dialogs Declaratively",
+        description = """
+        | You can open and close dialogs with JavaScript by toggling the `open` attribute, but you can also do it declaratively. Add the `data-dialog="open id"` to any button on the page, where `id` is the ID of the dialog you want to open.
+        """.stripMargin,
+        content = Source.annotate {
+          import io.github.nguyenyou.webawesome.laminar.{Dialog, Button}
+          div(
+            Dialog(
+              _.id := "dialog-opening", // [!code highlight]
+              _.label := "Dialog",
+            )("Lorem ipsum dolor sit amet, consectetur adipiscing elit."),
+            Button(
+              _.openDialog("dialog-opening") // [!code highlight]
+            )("Open Dialog")
+          )
+        }
+      )(),
     )
   }
-
 }
--- webawesome-laminar/src/io/github/nguyenyou/webawesome/laminar/Button.scala
+++ webawesome-laminar/src/io/github/nguyenyou/webawesome/laminar/Button.scala
@@ -122,6 +122,9 @@ object Button extends WebComponent("wa-button") {
   /** Used to override the form owner's `target` attribute. */
   lazy val formtarget: HtmlAttr[String] = stringAttr("formtarget")
 
+  def closeDialog(bool: Boolean) = L.dataAttr("dialog") := (if(bool) "close" else "")
+  def openDialog(id: String) = L.dataAttr("dialog") := s"open $id"
+
   // -- Slots --
 
   object slots {
