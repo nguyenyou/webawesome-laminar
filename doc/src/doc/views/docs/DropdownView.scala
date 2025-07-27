5a5e5a366654ed2561d5c3f88f1f1e8d2e03cbc9
nguyenyou <tunt081295@gmail.com>
2025-07-27 13:52:40 +0700

    🚢 ship it

--- doc/src/doc/views/docs/DropdownView.scala
+++ doc/src/doc/views/docs/DropdownView.scala
@@ -6,6 +6,7 @@ import doc.components.Locator.withLocator
 import doc.macros.Source
 import io.github.nguyenyou.webawesome.laminar.*
 import doc.components.Markdown
+import doc.libs.scalawind.*
 
 case class DropdownView()
     extends ExampleView(
@@ -251,6 +252,119 @@ case class DropdownView()
             DropdownItem(_.value := "pptx")("PowerPoint Presentation"),
             DropdownItem(_.value := "txt")("Plain Text"),
             DropdownItem(_.value := "json")("JSON File")
+          )
+        }
+      )().withLocator,
+      Demo(
+        title = "Distance",
+        description = """
+          |The distance from the panel to the trigger can be customized using the `distance` attribute. This value is specified in pixels.
+          """.stripMargin,
+        content = Source.annotate {
+          Dropdown(
+            _.distance := 30.0,
+            _.slots.trigger(
+              Button(_.withCaret := true)("Edit")
+            )
+          )(
+            DropdownItem()("Cut"),
+            DropdownItem()("Copy"),
+            DropdownItem()("Paste"),
+            Divider()(),
+            DropdownItem()("Find"),
+            DropdownItem()("Replace")
+          )
+        }
+      )().withLocator,
+      Demo(
+        title = "Offset",
+        description = """
+          |The offset of the panel along the trigger can be customized using the `skidding` attribute. This value is specified in pixels.
+          """.stripMargin,
+        content = Source.annotate {
+          Dropdown(
+            _.skidding := 30.0,
+            _.slots.trigger(
+              Button(_.withCaret := true)("Edit")
+            )
+          )(
+            DropdownItem()("Cut"),
+            DropdownItem()("Copy"),
+            DropdownItem()("Paste"),
+            Divider()(),
+            DropdownItem()("Find"),
+            DropdownItem()("Replace")
+          )
+        }
+      )().withLocator,
+      Demo(
+        title = "Submenus",
+        description = """
+          |To create submenus, nest [dropdown items](/docs/components/dropdown-item) inside of a dropdown item and assign `slot="submenu"` to each one. You can also add [dividers](/docs/components/divider) as needed.
+          """.stripMargin,
+        content = Source.annotate {
+          Dropdown(
+            _.slots.trigger(
+              Button(_.withCaret := true)("Export")
+            )
+          )(
+            DropdownItem(
+              _.slots.submenu(DropdownItem(_.value := "pdf")("PDF")),
+              _.slots.submenu(DropdownItem(_.value := "docx")("Word Document"))
+            )("Documents"),
+            DropdownItem(
+              _.slots.submenu(
+                DropdownItem(
+                  _.slots.submenu(DropdownItem(_.value := "xlsx")("Excel (.xlsx)")),
+                  _.slots.submenu(DropdownItem(_.value := "xls")("Excel 97-2003 (.xls)")),
+                  _.slots.submenu(DropdownItem(_.value := "csv")("CSV (.csv)"))
+                )("Excel Formats")
+              ),
+              _.slots.submenu(
+                DropdownItem(
+                  _.slots.submenu(DropdownItem(_.value := "ods")("OpenDocument (.ods)")),
+                  _.slots.submenu(DropdownItem(_.value := "tsv")("Tab-separated (.tsv)")),
+                  _.slots.submenu(DropdownItem(_.value := "json")("JSON (.json)"))
+                )("Other Formats")
+              ),
+              _.slots.submenu(DropdownItem(_.value := "numbers")("Apple Numbers"))
+            )("Spreadsheets"),
+            Divider()(),
+            DropdownItem(
+              _.slots.submenu(DropdownItem(_.`type` := "checkbox", _.value := "compress")("Compress files")),
+              _.slots.submenu(DropdownItem(_.`type` := "checkbox", _.checked := true, _.value := "metadata")("Include metadata")),
+              _.slots.submenu(DropdownItem(_.`type` := "checkbox", _.value := "password")("Password protect"))
+            )("Options")
+          )
+        }
+      )().withLocator,
+      Callout(
+        _.slots.icon(Icon(_.name := "circle-info")())
+      )(
+        "Dropdown items that have a submenu will not dispatch the `wa-select` event. However, items inside the submenu will, unless they also have a submenu."
+      ),
+      Callout(
+        _.variant := "warning",
+        _.slots.icon(Icon(_.name := "triangle-exclamation")())
+      )(
+        tw.mt2,
+        "As a UX best practice, avoid using more than one level of submenu when possible."
+      ),
+      Demo(
+        title = "Disabling Items",
+        description = """
+          |Add the `disabled` attribute to any [dropdown item](/docs/components/dropdown-item) to disable it.
+          """.stripMargin,
+        content = Source.annotate {
+          Dropdown(
+            _.slots.trigger(
+              Button(_.withCaret := true)("Payment method")
+            )
+          )(
+            DropdownItem(_.value := "cash")("Cash"),
+            DropdownItem(_.value := "check", _.disabled := true)("Personal check"),
+            DropdownItem(_.value := "credit")("Credit card"),
+            DropdownItem(_.value := "gift-card")("Gift card")
           )
         }
       )().withLocator
