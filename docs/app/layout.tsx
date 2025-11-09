a2a5e3254fa8c67bbd47d1033b84dd51dea25549
nguyenyou <tunt081295@gmail.com>
2025-11-09 15:46:20 +0700

    🚢 ship it

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -6,6 +6,16 @@ const inter = Inter({
   subsets: ['latin'],
 });
 
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  description: 'Laminar bindings for WebAwesome web components library',
+  title: {
+    template: '%s | WebAwesome Laminar',
+    default: 'WebAwesome Laminar', // a default is required when creating a template
+  },
+}
+
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
