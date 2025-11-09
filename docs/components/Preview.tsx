52fed1c60188e9a192e0c3a2308fb3c2a0600e90
nguyenyou <tunt081295@gmail.com>
2025-11-09 20:19:05 +0700

    tailwind

--- docs/components/Preview.tsx
+++ docs/components/Preview.tsx
@@ -2,7 +2,7 @@
 import { Frame } from "@ark-ui/react/frame";
 import { DynamicCodeBlock } from "fumadocs-ui/components/dynamic-codeblock";
 import { Tabs, Tab } from "fumadocs-ui/components/tabs";
-
+import { useRef } from "react";
 export const Preview = ({
   code,
   userCode,
@@ -15,13 +15,23 @@ export const Preview = ({
   height?: string;
 }) => {
   const h = height ?? "h-(--height)"
-
+  const ref = useRef<HTMLIFrameElement>(null)
   const srcDoc = `<html>
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="//cdn.jsdelivr.net/npm/@awesome.me/webawesome@3.0.0/dist-cdn/styles/themes/default.min.css" rel="stylesheet" />
 <base target=_blank>
+<style>
+  body {
+    padding: 4px;
+  }  
+</style>
+<style type="text/tailwindcss">
+@layer theme, base, components, utilities;
+@import "tailwindcss/theme.css" layer(theme);
+@import "tailwindcss/utilities.css" layer(utilities);
+</style>
 </head>
 <body style='overflow: hidden'>
  <div id="${exampleId}"></div>
@@ -39,9 +49,17 @@ export const Preview = ({
     <Tabs items={["Preview", "Code"]}>
       <Tab value="Preview" className="w-full">
         <Frame
+          ref={ref}
           title="Preview"
           className={`outline-none rounded-lg bg-fd-background w-full ${h}`}
           srcDoc={srcDoc}
+          onMount={() => {
+            const doc = ref.current?.contentDocument
+            if (!doc) return
+            const script = doc.createElement('script')
+            script.src = 'https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4'
+            doc.body.appendChild(script)
+          }}
         ></Frame>
       </Tab>
       <Tab value="Code" className="w-full">
