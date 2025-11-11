70f8a2b8cc258a56fdc9b12d97122bcc10d387ff
nguyenyou <tunt081295@gmail.com>
2025-11-11 10:52:44 +0700

    🚢 ship it

--- docs/mdx-components.tsx
+++ docs/mdx-components.tsx
@@ -1,11 +1,14 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
 import { Preview } from './components/Preview';
+import { Tab, Tabs } from 'fumadocs-ui/components/tabs';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     ...components,
     Preview: Preview,
+    Tab: Tab,
+    Tabs: Tabs,
   };
 }
